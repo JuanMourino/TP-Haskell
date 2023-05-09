c8158adb21c0143d6a2f6a5c0d93b415d2396721
Tomiitooo <132406068+Tomiitooo@users.noreply.github.com>
2023-05-09 19:48:45 -0300

    Add files via upload
    Aca esta como se me ocurrio hasta el ej 9, cuando puedan revisen en especial el 9 que es medio rebuscado.

--- iap1-tp.hs
+++ iap1-tp.hs
@@ -62,27 +62,43 @@ cantidadDeAmigos (_, (pu, su) : xs, _) u | pu == u || su == u = 1 + cantidadDeAm
 
 -- describir qué hace la función: .....
 usuarioConMasAmigos :: RedSocial -> Usuario
-usuarioConMasAmigos = undefined
+usuarioConMasAmigos ([usuario], _, _) = usuario
+usuarioConMasAmigos ((u1 : u2 : us), relaciones, _) | (cantidadDeAmigos ((u1: u2: us), relaciones, []) u1) > (cantidadDeAmigos ((u1: u2: us), relaciones, []) u2) = usuarioConMasAmigos ((u1 : us), relaciones, [])
+                                                    | otherwise = usuarioConMasAmigos ((u2 : us), relaciones, [])
 
 -- describir qué hace la función: .....
 estaRobertoCarlos :: RedSocial -> Bool
-estaRobertoCarlos = undefined
+estaRobertoCarlos (_, [], _) = False
+estaRobertoCarlos ([], _, _) = False
+estaRobertoCarlos (u:us, relaciones, _) = cantidadDeAmigos ([], relaciones, []) u >= 1000000 || estaRobertoCarlos (us, relaciones, [])
 
 -- describir qué hace la función: .....
 publicacionesDe :: RedSocial -> Usuario -> [Publicacion]
-publicacionesDe = undefined
+publicacionesDe (_, _, []) _ = []
+publicacionesDe (_, _, ((u, p, us) : ps)) usuario | u == usuario = (u, p, us) : publicacionesDe ([], [], ps) usuario
+                                                  | otherwise = publicacionesDe ([], [], ps) usuario
 
 -- describir qué hace la función: .....
 publicacionesQueLeGustanA :: RedSocial -> Usuario -> [Publicacion]
-publicacionesQueLeGustanA = undefined
+publicacionesQueLeGustanA (_, _, []) _ = []
+publicacionesQueLeGustanA (_, _, (publicacion : ps)) u | gustaPublicacion u publicacion = publicacion : publicacionesQueLeGustanA ([], [], ps) u
+                                                       | otherwise = publicacionesQueLeGustanA ([], [], ps) u
+
+--Ve la lista de usuarios a los que les gusta una publicacion e indica si un usuario dado esta dentro de esa lista
+gustaPublicacion :: Usuario -> Publicacion -> Bool
+gustaPublicacion _ (_, _, []) = False
+gustaPublicacion u (poster, _, (u1 : us)) = u == u1 || gustaPublicacion u (poster , [], us)
 
 -- describir qué hace la función: .....
 lesGustanLasMismasPublicaciones :: RedSocial -> Usuario -> Usuario -> Bool
-lesGustanLasMismasPublicaciones = undefined
+lesGustanLasMismasPublicaciones red u1 u2 = (publicacionesQueLeGustanA red u1) == (publicacionesQueLeGustanA red u2)
 
 -- describir qué hace la función: .....
 tieneUnSeguidorFiel :: RedSocial -> Usuario -> Bool
-tieneUnSeguidorFiel = undefined
+tieneUnSeguidorFiel (_, _, []) _ = False
+tieneUnSeguidorFiel (_, _, ((u1, _, us) : ps)) u | u1 == u && us == [] = False
+                                                 | u1 == u && publicacionesDe ([], [], (u1, [], us) : ps) u1 == publicacionesQueLeGustanA ([], [], (u1, [], us) : ps) (head us) = True
+                                                 | otherwise = tieneUnSeguidorFiel ([], [], ((u1, [], (tail us)) : ps)) u1
 
 -- describir qué hace la función: .....
 existeSecuenciaDeAmigos :: RedSocial -> Usuario -> Usuario -> Bool
